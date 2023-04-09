669a08b47d14d505cacc4438d91ac186d90c5e1c
VladislavDegtyarenko <vladislavdegtyarenko@gmail.com>
2023-04-09 17:48:54 +0300

    updated PDF CV to Mar23 version

--- src/sections/05_Contact.jsx
+++ src/sections/05_Contact.jsx
@@ -106,7 +106,7 @@ const Contact = () => {
             </Fade>
           </div>
           <div className="contact__download-cv">
-            <a href="Vladyslav_Dihtiarenko_CV_Feb23.pdf" target="_blank">
+            <a href="Vladyslav_Dihtiarenko_CV_Mar23.pdf" target="_blank">
               <DownloadIcon />
               Download CV
             </a>
