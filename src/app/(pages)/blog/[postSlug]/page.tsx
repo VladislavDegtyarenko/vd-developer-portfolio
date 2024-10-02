343d98c9d6650e0a20a45d135cba8165d323d7e4
Vladislav Degtyarenko <vladislavdegtyarenko@gmail.com>
2024-10-03 00:11:44 +0300

    isr 3 hrs

--- src/app/(pages)/blog/[postSlug]/page.tsx
+++ src/app/(pages)/blog/[postSlug]/page.tsx
@@ -6,7 +6,8 @@ import PostSkeleton from "./components/PostSkeleton";
 import BlogPostServer from "./components/BlogPostServer";
 
 export const dynamic = "force-static";
-export const revalidate = 3600; // 1 hour
+// export const revalidate = 3600; // 1 hour
+export const revalidate = 10800; // 3 hours
 // export const revalidate = 60; // 1 min
 // export const revalidate = 259200; // 3 days
 
