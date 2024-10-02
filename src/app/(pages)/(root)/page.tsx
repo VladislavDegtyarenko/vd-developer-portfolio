99c94926cb49940f6cc3f43770c3814fb683699d
Vladislav Degtyarenko <vladislavdegtyarenko@gmail.com>
2024-10-03 00:11:38 +0300

    return youtube section, use mocks on dev env

--- src/app/(pages)/(root)/page.tsx
+++ src/app/(pages)/(root)/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
       <About />
       <Experience />
       <Projects />
-      {/* <YouTubeSection /> */}
+      <YouTubeSection />
       <Reviews />
       <Contact />
       <ProjectPreview />
