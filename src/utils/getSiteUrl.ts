78ad053e40787afc72909fb72d2078ba273dd19c
Vladyslav Dihtiarenko <vladislavdegtyarenko@gmail.com>
2025-07-24 22:47:38 +0300

    test

--- src/utils/getSiteUrl.ts
+++ src/utils/getSiteUrl.ts
@@ -6,20 +6,12 @@ const normalizeUrl = (url: string) =>
 export const getSiteUrl = () => {
   const vercelEnv = process.env.VERCEL_ENV;
   const nodeEnv = process.env.NODE_ENV;
-  const vercelUrl = process.env.VERCEL_URL;
+  const vercelUrl = process.env.VERCEL_URL || "";
 
   console.table({ vercelEnv, nodeEnv, vercelUrl });
 
-  if (vercelUrl) {
-    const fullUrl = normalizeUrl(vercelUrl);
-    const isProduction =
-      vercelEnv === "production" || fullUrl.includes(PRODUCTION_DOMAIN);
-
-    if (isProduction) {
-      return PRODUCTION_DOMAIN;
-    }
-
-    return fullUrl;
+  if (vercelUrl && vercelEnv === "preview") {
+    return normalizeUrl(vercelUrl);
   }
 
   if (nodeEnv === "development") {
--- src/utils/isPreview.ts
+++ src/utils/isPreview.ts
@@ -1,3 +1,1 @@
-export const isPreview = () =>
-  process.env.VERCEL_ENV === "preview" ||
-  process.env.VERCEL_URL?.includes(".vercel.app");
+export const isPreview = () => process.env.VERCEL_ENV === "preview";
