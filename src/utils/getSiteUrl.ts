e81f2ea27497b0dfadbdac85b7e94b56f5ea58e6
Vladyslav Dihtiarenko <vladislavdegtyarenko@gmail.com>
2025-07-24 21:21:25 +0300

    test getSiteUrl

--- src/utils/getSiteUrl.ts
+++ src/utils/getSiteUrl.ts
@@ -1,7 +1,18 @@
 import { PRODUCTION_DOMAIN } from "@/constants";
 
 export const getSiteUrl = () => {
+  console.table({
+    "process.env.VERCEL_ENV": process.env.VERCEL_ENV,
+    "process.env.NODE_ENV": process.env.NODE_ENV,
+    "process.env.VERCEL_URL": process.env.VERCEL_URL,
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    return PRODUCTION_DOMAIN;
+  }
+
   // Prefer Vercel-provided environment variable
+
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
