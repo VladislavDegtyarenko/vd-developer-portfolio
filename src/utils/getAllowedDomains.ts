b49d1b0b712a37d0498dbc66fd99e63a4fcc42ad
Vladislav Degtyarenko <vladislavdegtyarenko@gmail.com>
2025-12-20 17:49:08 +0200

    add logs

--- src/utils/getAllowedDomains.ts
+++ src/utils/getAllowedDomains.ts
@@ -1,10 +1,14 @@
 export const getAllowedDomains = () => {
+  console.log("process.env.ALLOWED_DOMAINS: ", process.env.ALLOWED_DOMAINS);
+
   return (
     process.env.ALLOWED_DOMAINS?.split(",").map((domain) => domain.trim()) ?? []
   );
 };
 
 export const getAllowedHosts = () => {
+  console.log("process.env.ALLOWED_HOSTS: ", process.env.ALLOWED_HOSTS);
+
   return (
     process.env.ALLOWED_HOSTS?.split(",").map((host) => host.trim()) ||
     getAllowedDomains()
